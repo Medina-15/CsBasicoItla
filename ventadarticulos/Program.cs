2ca9f799ae3c6e9f56f3838b633668d1f14b6f3a
Medina <alisonmmr@gmail.com>
2024-11-01 22:50:42 -0400

    actualizacion1

---
+++ ventadarticulos/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ventadarticulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("En este programa digitas el precio de un articulo, luego la cantidad que se lleva el cliente y luego se muestra la cantidad que debe de abonar");
+
+            // Declarando variables
+
+
+            int precio = 0;
+            int cantidad = 0;
+            int resultado = 0;
+
+            // Recoleccion de datos
+            
+            Console.WriteLine("Digite el precio del articulo:");
+            precio = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriba la cantidad del articulo:");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
